f455af71e92f939e7096df35cefea4d63fd1fa00
sampatel91 <saumil5691@gmail.com>
2014-10-02 18:53:37 -0400

    blah

---
+++ jarvis.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+from argparse import ArgumentParser
+
+def main(argv):
+    parser = ArgumentParser(
+        usage="%(prog)s [option] <pathname> [option] <pathname>")
+    print (argv)
+
+if __name__ == "__main__":
+    main(sys.argv)
+
