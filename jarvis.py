93c75e3bfbc9f536617f1c2207061ba17bf6aa38
Luis Enrique Lopez archetti <luis@Luiss-MacBook-Pro.local>
2014-10-02 19:17:49 -0400

    test push

--- jarvis.py
+++ jarvis.py
@@ -1,12 +1,33 @@
-#!/usr/bin/python
-import sys
-from argparse import ArgumentParser
 
-def main(argv):
-    parser = ArgumentParser(
-        usage="%(prog)s [option] <pathname> [option] <pathname>")
-    print (argv)
+import sys, wave, struct, math, subprocess, os
+import numpy as np
+from scipy.fftpack import dct
 
-if __name__ == "__main__":
-    main(sys.argv)
 
+def main(file1, file2):
+	sys.exit(0)
+    
+
+
+
+
+def parseArgs(args):
+
+ if args[1] == '-f' and args[3] == '-f':
+ 	main(argv[2], argv[4])
+ 	sys.exit(0)
+
+
+
+if __name__ == '__main__':
+
+	if len(sys.argv) != 5:
+	 	sys.stderr.write('ERROR: Proper command line usage is one of:\n')
+	 	sys.exit(-1)	
+	else:
+    	 if not (sys.argv[1] == '-f' or sys.argv[3] == '-f'):
+    	 	sys.stderr.write('ERROR: Proper command line usage is one of:\n')
+         	sys.stderr.write(' ./p4500 -f <pathname> -f <pathname>\n')
+         	sys.exit(-1)
+    	 else:
+    	 	parseArgs(sys.argv)
