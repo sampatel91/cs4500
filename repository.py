d24b47662880fe87419532e6529c1c08ad664745
Kyle Vinagro <vinagro.k@husky.neu.edu>
2014-10-03 11:36:49 -0400

    added check_type

--- repository.py
+++ repository.py
@@ -2,4 +2,13 @@ import magic
 
 #Checks if file is actually .wav
 def check_type(file):
-	print 
+	mime = magic.open(magic.MIME_TYPE)
+	mime.load()
+	return mime.file(file)
+
+
+#Tests
+print check_type("audio/Sor3508.mp3")
+print check_type("C:\Users\Kyle\Dropbox\Documents\Git\Sor3508.wav")
+print check_type("audio/z01.wav")
+
