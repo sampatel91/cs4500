8e7979cc2f18e849e4f5fa0212c2fcbb44ad32fa
Marc J. Savino <marcs@rezon.ccs.neu.edu>
2014-10-09 17:34:02 -0400

    finished assignment 4 -SUP

--- utilities.py
+++ utilities.py
@@ -3,7 +3,7 @@ import wave
 import magic
 from array import array
 
-def checkArgs(args):
+def check_args(args):
     """
     Arguments: Array of command line arguments
     
@@ -13,38 +13,30 @@ def checkArgs(args):
     """
     if len(args) != 5:
         sys.stderr.write('ERROR: incorrect command line\n\n')
-        sys.stderr.write(' ./dan -f <pathname> -f <pathname>\n')
-        sys.stderr.write(' ./dan -f <pathname> -d <pathname>\n')
-        sys.stderr.write(' ./dan -d <pathname> -f <pathname>\n')
-        sys.stderr.write(' ./dan -d <pathname> -d <pathname>\n')
         sys.exit(-1)
     else:
         if not ((args[1] == '-f' or args[1] == '-d') and
                     (args[3] == '-f' or args[3] == '-d')):
             sys.stderr.write('ERROR: incorrect command line\n\n')
-            sys.stderr.write(' ./dan -f <pathname> -f <pathname>\n')
-            sys.stderr.write(' ./dan -f <pathname> -d <pathname>\n')
-            sys.stderr.write(' ./dan -d <pathname> -f <pathname>\n')
-            sys.stderr.write(' ./dan -d <pathname> -d <pathname>\n')
             sys.exit(-1)
 
 
-def getFileName(file):
+def get_file_name(filepath):
     """
     Arguments: filepath
     
     Returns the resource file name found at the end of the given file path
     """
-    fileName = file.split('/')
-    if len(fileName) == 0:
-        return fileName
+    file_name = filepath.split('/')
+    if len(file_name) == 0:
+        return file_name
     else:
-        return fileName[len(fileName) - 1]
+        return file_name[len(file_name) - 1]
 
 
-def is_wave_file(filename):
+def is_wave_file(filepath):
     """
-    Arguments: file
+    Arguments: filepath
     
     Checks if a file is in WAVE format.
     
@@ -52,40 +44,45 @@ def is_wave_file(filename):
     """
     mime = magic.open(magic.MIME_TYPE)
     mime.load()
-    if mime.file(filename) == ('audio/x-wav'):
+    if mime.file(filepath) == ('audio/x-wav'):
         return True
     else:
-        sys.stderr.write('ERROR: %s is not a supported format\n' % (file))
-        sys.exit(-1)
+        sys.stderr.write('ERROR: %s is not a supported format\n' % (filepath))
+        sys.exit(1)
 
 
-def get_length(file):
+def get_length(filepath):
     """
-    Arguments: file 
+    Arguments: filepath
     
     Returns the length of a WAVE file. 
     """
-    file = wave.open(file, 'r')
+    file = wave.open(filepath, 'r')
     frames = file.getnframes()
     frate = file.getframerate()
     duration = frames / frate
     return duration
 
 
-def read_file(file):
+def read_file(filepath):
     """ 
-    Arguments: file
+    Arguments: filepath
     
     Returns the raw data within the given WAVE file.
     """
-    file = wave.open(file, 'r')
+    file = wave.open(filepath, 'r')
     frames = file.getnframes()
     data = file.readframes(frames)
     return data
 
 
-def get_channel(file):
-    file = wave.open(file, 'r')
+def get_channel(filepath):
+    """
+    Argument: filepath
+
+    Returns 1 for mono and 2 for stereo channels.
+    """
+    file = wave.open(filepath, 'r')
     return file.getnchannels()
 
 
@@ -105,22 +102,22 @@ def string_to_array(string, channel):
         data = raw_data
     return data
 
-def compare(file1, file2):
+def compare(filepath1, filepath2):
     """
-    Arguments: file x file
+    Arguments: filepath x filepath
     
-    Compares two WAVE files. 
+    Compares two WAVE files found at the base of a file path 
     
     Returns TRUE if the two files MATCH. Otherwise returns FALSE.
     """
-    data1 = read_file(file1)
-    array1 = string_to_array(data1, get_channel(file1))
-    data2 = read_file(file2)
-    array2 = string_to_array(data2, get_channel(file2))
+    data1 = read_file(filepath1)
+    array1 = string_to_array(data1, get_channel(filepath1))
+    data2 = read_file(filepath2)
+    array2 = string_to_array(data2, get_channel(filepath2))
     i = 0
     while i < len(array1):
-        if not (array1[i] == array2[i]):
+        distance_var = array2[i] / array1[i]
+        if not (distance_var >= 0.97 and distance_var <= 1.03):
 	    return False
         i += 1
     return True
-
